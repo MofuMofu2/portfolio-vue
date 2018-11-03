0736273d0af8896d4435063f8e08c985ad5d714d
MofuMofu2 <froakie002@gmail.com>
2018-11-03 14:49:32 +0900

    [add] #1 Header表示に必要な諸々を追加
    scss利用する場合npmのnpm-sassがいるらしいんだけどうまくimportできていない

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
+  <title-header></title-header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import titleHeader from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
+    'title-header': titleHeader
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<script>
+export default {
+  name: 'titleHeader',
+  data () {
+    return {
+      title: '同人誌展示会場'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    background-color: #32BDED;
+    color: #E5E400;
+    height: 100px;
+  }
+</style>
