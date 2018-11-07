e4219c37abc5d3a82597c058ea52967e571efd65
MofuMofu2 <froakie002@gmail.com>
2018-11-07 13:36:04 +0900

    [add][update] #2 GitHubアイコンを追加
    リンクはクソリポジトリにした
    2個アイコンが並ぶとうるさいのでピクセル数を小さめに変更
    マージンがないとくっつきすぎなのでマージンを開けた

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="page-footer">
-    <a href="https://twitter.com/froakie0021"><img :src="image_src" alt="twitter"></a>
+    <a href="https://twitter.com/froakie0021"><img :src="twitter_src" alt="twitter"></a>
+    <a href="https://github.com/MofuMofu2/portfolio-vue"><img :src="github_src" alt="github"></a>
   </div>
 </template>
 
@@ -9,7 +10,8 @@ export default {
   name: 'pageFooter',
   data () {
     return {
-      image_src: require("../assets/Twitter_Social_Icon_Rounded_Square_Color.svg")
+      twitter_src: require("../assets/Twitter_Social_Icon_Rounded_Square_Color.svg"),
+      github_src: require("../assets/GitHub-Mark.svg")
     }
   }
 }
@@ -21,7 +23,8 @@ export default {
     height: 100px;
   }
   img {
-    height: 40px;
-    width: 40px;
+    height: 30px;
+    margin: 5px;
+    width: 30px;
   }
 </style>
