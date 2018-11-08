d80ba40b538452c8d9da5b9be681e4e9ca52fdaf
MofuMofu2 <froakie002@gmail.com>
2018-11-08 13:09:26 +0900

    [add] #4 クソタイトル表示コンポーネントを作成
    後々keyとidで切り替えができるように、v-bindでデータを表示するようにした

---
+++ src/components/BookTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="book-title">
+    <div class="fanbook-title">
+      <fan-book v-bind:title="item">{{ item }}</fan-book>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bookTitle',
+  data() {
+    return {
+      item: "ログと情報をレッツ・ラ・まぜまぜ！～ELK Stack で作るBI環境～"
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .book-title {
+    background-color: #E5E400;
+    color: #32BDED;
+    font-size: 30px;
+  }
+  .fanbook-title {
+    width: 520px;
+  }
+</style>
