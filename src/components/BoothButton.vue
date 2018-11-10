31c3294ff25d409cf0ddde164ecadf389c6d6ea9
MofuMofu2 <froakie002@gmail.com>
2018-11-10 17:07:38 +0900

    [add] #8 電子版リンクを追加

--- src/components/BoothButton.vue
+++ src/components/BoothButton.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="booth-button">aaa</div>
+  <div class="booth-button">
+    <a href="booth.url">電子版をポチる</a>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'boothButton'
+  name: 'boothButton',
+  data() {
+    return {
+      booth: [
+        {
+          url: 'https://booth.pm/ja/items/490460'
+        }
+      ]
+    }
+  }
 }
 </script>
 
