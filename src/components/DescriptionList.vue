af3b81e3b816813e788b79040108347615411e81
MofuMofu2 <froakie002@gmail.com>
2018-11-10 14:23:54 +0900

    [add] #6 KUSOポイントコンポーネントの追加

---
+++ src/components/DescriptionList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="description-list">
+    <div class="overview">
+      <h3>KUSO POINT</h3>
+      <ul>
+        <li>KUSO</li>
+      </ul>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'descriptionList'
+}
+</script>
+
+<style scoped>
+
+</style>
