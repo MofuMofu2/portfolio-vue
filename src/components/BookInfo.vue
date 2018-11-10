4af90c55d412b1f9ac3c289531ea9207d59a1b2b
MofuMofu2 <froakie002@gmail.com>
2018-11-10 16:00:54 +0900

    [add] #7 関連URL用のあれこれを追加
    for文でやるのがいいか、もっといい方法があるのかがわからない

--- src/components/BookInfo.vue
+++ src/components/BookInfo.vue
@@ -7,6 +7,9 @@
           {{ info.caption }}
         </div>
       </div>
+      <div class="posts-info">
+        <h4>関連URL</h4>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +23,11 @@ export default {
         { caption: '2017年' },
         { caption: '4月'},
         { caption: '技術書典2' }
+      ],
+      posts: [
+        { link: 'http://rimarimadan.hatenablog.com/entry/2017/04/12/%E6%8A%80%E8%A1%93%E6%9B%B8%E5%85%B82_%E3%82%82%E3%81%B5%E3%82%82%E3%81%B5%E3%81%A1%E3%82%83%E3%82%93%E3%81%AE%E6%88%A6%E3%81%84%E5%B1%A5%E6%AD%B4'},
+        { link: 'http://amzn.asia/d/be7bGtk'},
+        { link: 'https://booth.pm/ja/items/490460'}
       ]
     }
   }
@@ -32,7 +40,6 @@ export default {
     line-height: 26px;
   }
   .published-info {
-    float: left;
     margin-right: 5px;
   }
   h4 {
