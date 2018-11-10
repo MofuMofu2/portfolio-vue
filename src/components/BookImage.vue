62665473014c70ad3686b736810122aa61e78163
MofuMofu2 <froakie002@gmail.com>
2018-11-10 10:41:09 +0900

    [update] #3 CSSをあてて画像の大きさを調整
    後々画像を2枚に並べたいので、1枚のwidthは明確に指定した

--- src/components/BookImage.vue
+++ src/components/BookImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="book-image">
+  <div class="book-image">
     <img :src="image_src" alt="bookImage">
   </div>
 </template>
@@ -16,5 +16,13 @@ export default {
 </script>
 
 <style scoped>
-
+  .book-image {
+    height: 400px;
+    margin-top: 30px;
+    width: 570px;
+  }
+  img {
+    height: 100%;
+    width: 285px;
+  }
 </style>
