b838ae1a746bb9f1c50516d5036287aba293d3cf
MofuMofu2 <froakie002@gmail.com>
2018-11-10 14:39:06 +0900

    [update] #6 クソポイントをリスト形式 + v-forで描画
    v-forはなるべくkey属性を持つことが推奨されている

--- src/components/DescriptionList.vue
+++ src/components/DescriptionList.vue
@@ -3,7 +3,9 @@
     <div class="overview">
       <h3>KUSO POINT</h3>
       <ul>
-        <li>KUSO</li>
+        <li v-for="(item, key) in overviews" v-bind:key="item.id">
+          {{ item.promotion }}
+        </li>
       </ul>
     </div>
     </div>
@@ -11,7 +13,16 @@
 
 <script>
 export default {
-  name: 'descriptionList'
+  name: 'descriptionList',
+  data() {
+    return {
+      overviews: [
+        { promotion: 'ふざけたタイトル' },
+        { promotion: 'なにも伝わってこない表紙' },
+        { promotion: '布教本のはずなのにまさかの省略記法を使っている' }
+      ]
+    }
+  }
 }
 </script>
 
